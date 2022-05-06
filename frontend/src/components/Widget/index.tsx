fe2febce3c4954fa2c98f3c6b2ff19d86128f603
Gui <guiggff@gmail.com>
2022-05-06 16:51:34 -0300

    :tada: First commit

---
+++ frontend/src/components/Widget/index.tsx
@@ -0,0 +1,9 @@
+import { ChatTeardropDots } from "phosphor-react";
+
+export const Widget: React.FC = () => {
+  return (
+    <button>
+      <ChatTeardropDots className="w" />
+    </button>
+  );
+};
